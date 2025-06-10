f6378e1d54d3ed213cf08b65a0705c81b75d6ee8
hamatank <sapsal7383@gmail.com>
2025-06-10 16:23:18 +0900

    카드 재구성

--- js/card.js
+++ js/card.js
@@ -1,23 +1,40 @@
-window.addEventListener('DOMContentLoaded', function() {
-  showContent('recruit_box1');
+// window.addEventListener('DOMContentLoaded', function () {
+//   showContent('post-list1');
+// });
+
+// function showContent(content) {
+//   var postList1 = document.getElementById("post-list1");
+//   var postList2 = document.getElementById("post-list2");
+//   var postList3 = document.getElementById("post-list3");
+
+//   // 모든 리스트 숨김
+//   postList1.style.display = "none";
+//   postList2.style.display = "none";
+//   postList3.style.display = "none";
+
+//   // 선택한 리스트 보이기
+//   if (content === "post-list1") {
+//     postList1.style.display = "block";
+//   } else if (content === "post-list2") {
+//     postList2.style.display = "block";
+//   } else if (content === "post-list3") {
+//     postList3.style.display = "block";
+//   }
+// }
+
+window.addEventListener('DOMContentLoaded', function () {
+  showContent('post-list1');
 });
 
 function showContent(content) {
-  var recruit_box1 = document.getElementById("recruit_box1");
-  var recruit_box2 = document.getElementById("recruit_box2");
-  var recruit_box3 = document.getElementById("recruit_box3");
+  var allPostLists = document.querySelectorAll('[id^="post-list"]');
+  allPostLists.forEach(function(postList) {
+    postList.style.display = 'none';
+  });
 
-  // 내용 숨김
-  recruit_box1.style.display = "none";
-  recruit_box2.style.display = "none";
-  recruit_box3.style.display = "none";
-
-  // 선택한 내용 보이기
-  if (content === "recruit_box1") {
-    recruit_box1.style.display = "block";
-  } else if (content === "recruit_box2") {
-    recruit_box2.style.display = "block";
-  } else if (content === "recruit_box3") {
-    recruit_box3.style.display = "block";
+  var selectedPostList = document.getElementById(content);
+  if (selectedPostList) {
+    selectedPostList.style.display = 'flex';  /* Show the selected post list with flex layout */
   }
 }
+
