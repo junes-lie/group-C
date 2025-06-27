81ac93237f4122194d849e9970b954c5dfefb57c
hamatank <sapsal7383@gmail.com>
2025-06-27 16:03:14 +0900

    2025/06/27
    1234

--- js/card.js
+++ js/card.js
@@ -1,29 +1,29 @@
 window.addEventListener('DOMContentLoaded', function () {
-  showContent('post-list1');
+  showContent('post-list');
 });
 
 function showContent(content) {
-  var postList1 = document.getElementById("post-list1");
-  var postList2 = document.getElementById("post-list2");
-  var postList3 = document.getElementById("post-list3");
+  var postList1 = document.getElementById("post-list");
+  var postList2 = document.getElementById("post-list");
+  var postList3 = document.getElementById("post-list");
 
   // 모든 리스트 숨김
-  postList1.style.display = "none";
-  postList2.style.display = "none";
-  postList3.style.display = "none";
+  postList.style.display = "none";
+  postList.style.display = "none";
+  postList.style.display = "none";
 
   // 선택한 리스트 보이기
-  if (content === "post-list1") {
-    postList1.style.display = "block";
-  } else if (content === "post-list2") {
-    postList2.style.display = "block";
-  } else if (content === "post-list3") {
-    postList3.style.display = "block";
+  if (content === "post-list") {
+    postList.style.display = "block";
+  } else if (content === "post-list") {
+    postList.style.display = "block";
+  } else if (content === "post-list") {
+    postList.style.display = "block";
   }
 }
 
 window.addEventListener('DOMContentLoaded', function () {
-  showContent('post-list1');
+  showContent('post-list');
 });
 
 function showContent(content) {
@@ -37,4 +37,3 @@ function showContent(content) {
     selectedPostList.style.display = 'flex';  /* Show the selected post list with flex layout */
   }
 }
-
